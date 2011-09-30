6026be025ae12838df96cf55858c06583dff6b04
Isaac <icambron@alum.mit.edu>
2011-09-30 05:37:36 -0400

    dashes in post summaries

--- views/post_summary.rb
+++ views/post_summary.rb
@@ -4,7 +4,7 @@ class PostSummary < Base
   def content
     a(:href => @post.path) do
       span cal_date(@post.date), :class => "post_summary_date"
-      span " " + @post.title, :class => "post_summary_title"
+      span " - " + @post.title, :class => "post_summary_title"
     end
   end
 end
